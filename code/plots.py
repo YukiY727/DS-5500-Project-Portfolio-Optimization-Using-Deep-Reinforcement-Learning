5d80fd19435dab058d631a08e550f30181550ee3
YukiY727 <y.yamada@4nonome.com>
2024-06-17 17:58:38 +0900

    save_pathを指定できるように変更

--- code/plots.py
+++ code/plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #function to plot cumulative portfolio values
-def plot_cpv(final_pf_values_opt, final_pf_values_eq, initial_portfolio_value, plot_name):
+def plot_cpv(final_pf_values_opt, final_pf_values_eq, initial_portfolio_value, plot_name, save_path='test_result_plots/'):
 	plt.plot(final_pf_values_opt, label = 'optimizing agent')
 	plt.plot(final_pf_values_eq, label = 'equiweight agent')
 	plt.plot([initial_portfolio_value] * len(final_pf_values_opt), label = 'no investment')
@@ -10,15 +10,15 @@ def plot_cpv(final_pf_values_opt, final_pf_values_eq, initial_portfolio_value, p
 	plt.title('cumulative portfolio values over test steps')
 	plt.xlabel('test steps')
 	plt.ylabel('cumulative portfolio value')
-	plt.savefig('test_result_plots/' + plot_name)
+	plt.savefig(save_path + plot_name)
 	plt.clf()
 
 #function to plot final wt vectors assigned
-def plot_wts_assigned(wt_vector, num_stocks, ticker_list, plot_name):
+def plot_wts_assigned(wt_vector, num_stocks, ticker_list, plot_name, save_path='test_result_plots/'):
 	plt.bar(np.arange(num_stocks + 1), wt_vector)
 	plt.title('Final Portfolio weights (test set)')
 	plt.xticks(np.arange(num_stocks + 1), ['Cash'] + ticker_list, rotation=45)
 	plt.xlabel('tickers')
-	plt.savefig('test_result_plots/' + plot_name)
+	plt.savefig(save_path + plot_name)
 	plt.clf()
 
